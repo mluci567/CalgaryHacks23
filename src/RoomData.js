72988b8357d4d94ee819427d5a5ca1a601362d58
matthew-hre <whycardboard@gmail.com>
2023-02-18 20:47:40 -0700

    added room data

--- src/RoomData.js
+++ src/RoomData.js
@@ -1,12 +1,21 @@
+import foodIcon from "./assets/cutlery.png";
+import disabledIcon from "./assets/disabled.png";
+import energyIcon from "./assets/energy.png";
+import parkingIcon from "./assets/parking.png";
+import volumeIcon from "./assets/volume.png";
+import washroomIcon from "./assets/bathroom.png";
+import lightIcon from "./assets/light-bulb.png";
+import presentationIcon from "./assets/presentation.png";
+
 const RoomData = ({
-  roomNumber,
+  roomnumber,
   floor,
   type,
   capacity,
   hours,
-  accessRating,
-  noiseLevel,
-  adjustableLighting,
+  accessrating,
+  noiselevel,
+  adjustablelighting,
   whiteboard,
   outlets,
   food,
@@ -14,23 +23,235 @@ const RoomData = ({
   parking,
   transit,
 }) => {
-  <div className="roomData">
-    <h3>
-      {roomNumber} - {type}
-    </h3>
-    <p>Floor {floor}</p>
-    <p>Capacity: {capacity}</p>
-    <p>Hours: {hours}</p>
-    <p>Access Rating: {accessRating}</p>
-    <p>Noise Level: {noiseLevel}</p>
-    <p>Adjustable Lighting: {adjustableLighting}</p>
-    <p>Whiteboard: {whiteboard}</p>
-    <p>Outlets: {outlets}</p>
-    {food && <p>Food: {food}</p>}
-    {washroom && <p>Washroom: {washroom}</p>}
-    {parking && <p>Parking: {parking}</p>}
-    {transit && <p>Transit: {transit}</p>}
-  </div>;
+  return (
+    <div className="roomData">
+      <div className="roomHeader">
+        <h3>
+          {roomnumber} - {type}
+        </h3>
+      </div>
+      <hr />
+      <h4>Details</h4>
+      <p>
+        Floor {floor}, {hours}
+      </p>
+      <p>Capacity: {capacity}</p>
+      <hr />
+      <h4>Amenities</h4>
+      {adjustablelighting && (
+        <img
+          src={lightIcon}
+          alt="light icon"
+          title="Adjustable Lighting Avaliable"
+        ></img>
+      )}
+      {whiteboard && (
+        <img
+          src={presentationIcon}
+          alt="presentation icon"
+          title="White/Blackboards Avaliable"
+        ></img>
+      )}
+      {food && <img src={foodIcon} alt="food icon" title="Food Nearby"></img>}
+      {washroom && (
+        <img
+          src={washroomIcon}
+          alt="washroom icon"
+          title="Washrooms Nearby"
+        ></img>
+      )}
+      {parking && (
+        <img
+          src={disabledIcon}
+          alt="disabled icon"
+          title="Parking Nearby"
+        ></img>
+      )}
+      {transit && (
+        <img
+          src={parkingIcon}
+          alt="power icon"
+          title="Transit Accessable"
+        ></img>
+      )}
+      {outlets && (
+        <img src={energyIcon} alt="energy icon" title="Outlets Avaliable"></img>
+      )}
+      <hr />
+      <h4>Noise Level</h4>
+      {/* draw a volume icon for noise level */}
+      {noiselevel === "1" && (
+        <div className="rating">
+          <img src={volumeIcon} alt="volume icon" title="Low Volume Area"></img>
+        </div>
+      )}
+      {noiselevel === "2" && (
+        <div className="rating">
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Light Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Light Volume Area"
+          ></img>
+        </div>
+      )}
+      {noiselevel === "3" && (
+        <div className="rating">
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Average Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Average Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Average Volume Area"
+          ></img>
+        </div>
+      )}
+      {noiselevel === "4" && (
+        <div className="rating">
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Moderate Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Moderate Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Moderate Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Moderate Volume Area"
+          ></img>
+        </div>
+      )}
+      {noiselevel === "5" && (
+        <div className="rating">
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Loud Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Loud Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Loud Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Loud Volume Area"
+          ></img>
+          <img
+            src={volumeIcon}
+            alt="volume icon"
+            title="Loud Volume Area"
+          ></img>
+        </div>
+      )}
+      <p className="hideMe">-</p>
+      <hr />
+      <h4>Accessibility</h4>
+
+      {accessrating === "1" && (
+        <div className="rating">
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Poor Accessibility"
+          ></img>
+        </div>
+      )}
+      {accessrating === "2" && (
+        <div className="rating">
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Low Accessibility"
+          ></img>
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Low Accessibility"
+          ></img>
+        </div>
+      )}
+      {accessrating === "3" && (
+        <div className="rating">
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Average Accessibility"
+          ></img>
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Average Accessibility"
+          ></img>
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Average Accessibility"
+          ></img>
+        </div>
+      )}
+      {accessrating === "4" && (
+        <div className="rating">
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Good Accessibility"
+          ></img>
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Good Accessibility"
+          ></img>
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Good Accessibility"
+          ></img>
+          <img
+            src={disabledIcon}
+            alt="energy icon"
+            title="Good Accessibility"
+          ></img>
+        </div>
+      )}
+      {accessrating === "5" && (
+        <div className="rating">
+          <img src={disabledIcon} alt="energy icon" title="Accessible"></img>
+          <img src={disabledIcon} alt="energy icon" title="Accessible"></img>
+          <img src={disabledIcon} alt="energy icon" title="Accessible"></img>
+          <img src={disabledIcon} alt="energy icon" title="Accessible"></img>
+          <img src={disabledIcon} alt="energy icon" title="Accessible"></img>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RoomData;
--- src/pages/FindRoom.js
+++ src/pages/FindRoom.js
@@ -1,10 +1,68 @@
 import { useOutletContext } from "react-router-dom";
+import RoomData from "../RoomData";
 
 const FindRoom = () => {
   useOutletContext();
   return (
     <div className="FindRoom">
-      <h1>Find a Room</h1>
+      <div className="roomList">
+        <RoomData
+          roomnumber={"B1204"}
+          type={"Beeg Room"}
+          floor={"3"}
+          capacity={"2-6"}
+          hours={"8am-10pm"}
+          accessrating={"3"}
+          noiselevel={"2"}
+          adjustablelighting={"Yes"}
+          whiteboard={"Yes"}
+          outlets={"Yes"}
+          washroom={"Yes"}
+          transit={"Yes"}
+        />
+        <RoomData
+          roomnumber={"T111"}
+          type={"Presentation Room"}
+          floor={"1"}
+          capacity={"2-6"}
+          hours={"8am-5pm"}
+          accessrating={"5"}
+          noiselevel={"1"}
+          adjustablelighting={"No"}
+          whiteboard={"No"}
+          outlets={"Yes"}
+          food={"Yes"}
+          washroom={"Yes"}
+          parking={"Yes"}
+          transit={"Yes"}
+        />
+        <RoomData
+          roomnumber={"EA1204"}
+          type={"Smol Room"}
+          floor={"3"}
+          capacity={"2-6"}
+          hours={"8am-10pm"}
+          accessrating={"3"}
+          noiselevel={"2"}
+          adjustablelighting={"Yes"}
+          whiteboard={"Yes"}
+          outlets={"Yes"}
+        />
+        <RoomData
+          roomnumber={"J122"}
+          type={"Presentation Room"}
+          floor={"1"}
+          capacity={"2-6"}
+          hours={"8am-5pm"}
+          accessrating={"5"}
+          noiselevel={"1"}
+          adjustablelighting={"No"}
+          whiteboard={"No"}
+          outlets={"Yes"}
+          food={"Yes"}
+          washroom={"Yes"}
+        />
+      </div>
     </div>
   );
 };
